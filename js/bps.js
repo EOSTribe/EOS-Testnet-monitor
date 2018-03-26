5f328d4b12d030322d919f926f9585f2f9480fd0
eluzgin <eugene.luzgin@consensuscorp.com>
2018-03-26 17:04:11 -0600

    Pull updates from CryptoLions.io

---
+++ js/bps.js
@@ -0,0 +1,147 @@
+/**********************************************
+*
+* EOSNet.io
+* https://github.com/EOSTribe/EOS-Testnet-monitor
+* forked from https://github.com/CryptoLions/EOS-Testnet-monitor
+*
+***********************************************/
+
+
+var blockProducerList = [
+  {
+    "bp_name": "augustus",
+    "organisation": "EOSNet.io",
+    "location": "Bluffdale, UT",
+    "node_addr": "eosslc.com",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "tiberius",
+    "organisation": "EOS Sweden",
+    "location": "Västerås, Sweden",
+    "node_addr": "79.136.20.170",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "claudius",
+    "organisation": "EOS Russia",
+    "location": "Novosibirsk, Russia",
+    "node_addr": "eosio.ru",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "galba",
+    "organisation": "EOS Africa",
+    "location": "Johannesburg, South Africa",
+    "node_addr": "156.38.160.91",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "otho",
+    "organisation": "EOS NY",
+    "location": "New York, NY",
+    "node_addr": "eosnet.eosnewyork.io",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "9875"
+  },
+  {
+    "bp_name": "titus",
+    "organisation": "EOS London",
+    "location": "London, UK",
+    "node_addr": "bp2-d3.eos42.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "domitian",
+    "organisation": "CryptoLions.io",
+    "location": "Kiev, Ukraine",
+    "node_addr": "eosnettestnet.cryptolions.io",
+    "port_http": "8909",
+    "port_ssl": "",
+    "port_p2p": "9897"
+  },
+  {
+    "bp_name": "nerva",
+    "organisation": "EOS Detroit",
+    "location": "Columbus, Ohio",
+    "node_addr": "eosnet.eosdetroit.com",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "trajan",
+    "organisation": "EOS Rio",
+    "location": "São Paulo, Brazil",
+    "node_addr": "54.233.222.22",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "antoninus",
+    "organisation": "EOSdac",
+    "location": "Dublin, Ireland",
+    "node_addr": "ceos.blockmaker.io",
+    "port_http": "8600",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "marcus",
+    "organisation": "BP Node",
+    "location": "Montreal, Canada",
+    "node_addr": "node-1.bpnode.com",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "septimius",
+    "organisation": "EOS Node One",
+    "location": "Seoul, South Korea",
+    "node_addr": "13.209.19.204",
+    "port_http": "8800",
+    "port_ssl": "",
+    "port_p2p": "6600"
+  },
+  {
+    "bp_name": "severus",
+    "organisation": "EOSBrazil.com",
+    "location": "São Paulo, Brazil",
+    "node_addr": "eosbrazil.com",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9877"
+  },
+  {
+    "bp_name": "hadrian",
+    "organisation": "EOS Green",
+    "location": "Warsaw, Poland",
+    "node_addr": "54.37.139.93",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  }/*,
+  {
+    "bp_name": "publius",
+    "organisation": "EOSNet.io",
+    "location": "Bluffdale, UT, USA",
+    "node_addr": "eosslc.com",
+    "port_http": "8814",
+    "port_ssl": "",
+    "port_p2p": "6614"
+  }*/
+
+]
